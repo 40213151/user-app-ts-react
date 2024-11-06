a4e432d66efd028e254a4cb01b4d4f493aa99ee4
daimaru <daimaruplay@gmail.com>
2024-11-06 12:15:32 +0900

    Initial commit

---
+++ src/components/pages/UserManagement.tsx
@@ -0,0 +1,54 @@
+import { memo, useCallback, useEffect, VFC } from "react";
+import {
+  Center,
+  Spinner,
+  Wrap,
+  WrapItem,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/users/UserCard";
+import { UserDetailModal } from "../organisms/users/UserDetailModal";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { useSelectUser } from "../../hooks/useSelectUser";
+
+export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { getUsers, users, loading } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
+
+  useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users });
+      onOpen();
+    },
+    [users, onSelectUser, onOpen]
+  );
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                id={user.id}
+                onClick={onClickUser}
+                imageUrl="https://picsum.photos/800"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} user={selectedUser} />
+    </>
+  );
+});
